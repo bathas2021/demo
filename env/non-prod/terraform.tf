9f957f75550e55bec2dedad72c126d5ca2782395
bathas2021 <96002357+bathas2021@users.noreply.github.com>
2025-09-21 21:42:24 +0530

    Create terraform.tf

---
+++ env/non-prod/terraform.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_ecr_repository" "my_repo" {
+  name = "my-app-repo"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  image_tag_mutability = "MUTABLE"
+}
