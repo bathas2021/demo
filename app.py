04a7996a0afb63b40a59c764b57b5d2db720fbfb
bathas2021 <96002357+bathas2021@users.noreply.github.com>
2025-09-22 10:57:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,15 +6,26 @@ app = Flask(__name__)
 
 @app.route("/info")
 def info():
+    headers = {}
     try:
-        response = requests.get("http://169.254.169.254/latest/meta-data/instance-id", timeout=1)
+
+        token = requests.put(
+            "http://169.254.169.254/latest/api/token",
+            headers={"X-aws-ec2-metadata-token-ttl-seconds": "21600"},
+            timeout=1
+        ).text
+        headers = {"X-aws-ec2-metadata-token": token}
+    except Exception:
+        pass  
+    try:
+        response = requests.get("http://169.254.169.254/latest/meta-data/instance-id", headers=headers,timeout=1)
         response.raise_for_status()
         instance_id = response.text
     except Exception:
         instance_id = "not running on an EC2 instance"
 
     try:
-        response = requests.get("http://169.254.169.254/latest/meta-data/placement/availability-zone", timeout=1)
+        response = requests.get("http://169.254.169.254/latest/meta-data/placement/availability-zone", headers=headers,timeout=1)
         response.raise_for_status()
         availability_zone = response.text
     except Exception:
